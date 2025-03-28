d6e3c6c5254e238d95652820b11d0b5072f07a37
Mustafa Al-Saffar <mustafa.alsaffar@hotmail.com>
2025-03-28 10:17:50 +0100

    Update Mustafa.java

--- Mustafa.java
+++ Mustafa.java
@@ -2,6 +2,15 @@ public class Mustafa {
     public static void main(String[] args) {
 
         System.out.println("Hello Mustafa!");
+        Food();
+        Drink();
 
+
+    }
+    public static void Food() {
+        System.out.println("Mustafa love Pizza");
+    }
+    public static void Drink() {
+        System.out.println("Mustafa love Coca Cola");
     }
 }
