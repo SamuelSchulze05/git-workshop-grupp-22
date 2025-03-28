25194adf927e85f050ec06a2f33f5a5dfa7daf37
SamuelSchulze05 <samuel.sch05@outlook.com>
2025-03-28 10:16:05 +0100

    Update Samuel.java

--- Samuel.java
+++ Samuel.java
@@ -4,12 +4,12 @@ public class Samuel {
         System.out.println("Samuel");
     }
 
-    public void SamuelFood()
+    public static void SamuelFood()
     {
         System.out.println("Samuels favorit mat är kött");
     }
 
-    public void SamuelDrink()
+    public static void SamuelDrink()
     {
         System.out.println("Samuels favorit drink är pepsi");
     }
