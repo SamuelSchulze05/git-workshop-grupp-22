db1a8d71f7152b855b3bd10e97f5437869f58322
Josef Zakaria Jehia <josef.zakaria@outlook.com>
2025-03-28 10:33:56 +0100

    o

--- Alper.java
+++ Alper.java
@@ -1,6 +1,6 @@
 import javax.swing.*;
 
-public class alper {
+public class Alper {
     public static void main (String[] args) {
         String name = JOptionPane.showInputDialog("Vad heter du?");
         JOptionPane.showMessageDialog(null, "Hej " + name + "!");
